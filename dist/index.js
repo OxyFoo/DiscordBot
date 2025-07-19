d791cc97980a743197d6e5fd7e52209cd1206409
Gerem66 <geremy.lecaplain66@gmail.com>
2025-07-19 01:56:00 -0400

    refactor: Convert code to ES2022 syntax and improve async functions

--- dist/index.js
+++ dist/index.js
@@ -1,48 +1,33 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OxyBotFeature = void 0;
-require("dotenv/config");
-const rest_1 = require("@discordjs/rest");
-const v10_1 = require("discord-api-types/v10");
-const discord_js_1 = require("discord.js");
-const feature_js_1 = __importDefault(require("./src/feature.js"));
-exports.OxyBotFeature = feature_js_1.default;
-const utils_js_1 = require("./src/utils.js");
+import 'dotenv/config';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
+import { GatewayIntentBits, Partials, Client } from 'discord.js';
+import OxyBotFeature from './src/feature.js';
+import { Sleep } from './src/utils.js';
 const DEFAULT_DISCORD_OPTIONS = {
     intents: [
-        discord_js_1.GatewayIntentBits.Guilds,
-        discord_js_1.GatewayIntentBits.GuildMessages,
-        discord_js_1.GatewayIntentBits.MessageContent,
-        discord_js_1.GatewayIntentBits.DirectMessages
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages
     ],
     partials: [
-        discord_js_1.Partials.Channel
+        Partials.Channel
     ]
 };
 class OxyBot {
+    token = process.env.DISCORD_TOKEN || null;
+    classes = [];
+    client;
     /**
      * @param token Default set to process.env.DISCORD_TOKEN
      * @param options Default set to DEFAULT_DISCORD_OPTIONS
      */
     constructor(token = null, options = null) {
-        this.token = process.env.DISCORD_TOKEN || null;
-        this.classes = [];
         if (token !== null) {
             this.token = token;
         }
-        this.client = new discord_js_1.Client(options || DEFAULT_DISCORD_OPTIONS);
+        this.client = new Client(options || DEFAULT_DISCORD_OPTIONS);
         this.client.on('ready', this.onReady.bind(this));
         this.client.on('messageCreate', this.onMessage.bind(this));
         this.client.on('interactionCreate', this.onInteraction.bind(this));
@@ -61,24 +46,21 @@ class OxyBot {
         this.client.login(this.token);
         process.addListener('SIGINT', this.destructor.bind(this));
     }
-    destructor() {
-        return __awaiter(this, void 0, void 0, function* () {
-            console.log('\nExiting...');
-            process.removeAllListeners('SIGINT');
-            for (let i = 0; i < this.classes.length; i++) {
-                yield this.classes[i].onClose();
-            }
-            this.client.destroy();
-            yield (0, utils_js_1.Sleep)(1000);
-            process.exit(0);
-        });
+    async destructor() {
+        console.log('\nExiting...');
+        process.removeAllListeners('SIGINT');
+        for (let i = 0; i < this.classes.length; i++) {
+            await this.classes[i].onClose();
+        }
+        this.client.destroy();
+        await Sleep(1000);
+        process.exit(0);
     }
     /**
      * @description Event called when the bot is ready.
      */
     onReady(client) {
-        var _a;
-        console.log(`Logged in as '${(_a = client.user) === null || _a === void 0 ? void 0 : _a.tag}'!`);
+        console.log(`Logged in as '${client.user?.tag}'!`);
         this.InitSlashCommands();
         for (let i = 0; i < this.classes.length; i++) {
             this.classes[i].onReady();
@@ -104,30 +86,29 @@ class OxyBot {
      * @description Initialize slash commands for the bot.
      * @throws Error if the client user or token is not set.
      */
-    InitSlashCommands() {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (this.client.user === null) {
-                throw new Error('Client user is not set, cannot initialize slash commands.');
+    async InitSlashCommands() {
+        if (this.client.user === null) {
+            throw new Error('Client user is not set, cannot initialize slash commands.');
+        }
+        if (this.token === null) {
+            throw new Error('Token is not set, cannot initialize slash commands.');
+        }
+        const rest = new REST({ version: '10' }).setToken(this.token);
+        try {
+            const commands = [];
+            for (let i = 0; i < this.classes.length; i++) {
+                const classe = this.classes[i];
+                const slashCommands = await classe.DefineSlashCommands();
+                commands.push(...slashCommands);
             }
-            if (this.token === null) {
-                throw new Error('Token is not set, cannot initialize slash commands.');
-            }
-            const rest = new rest_1.REST({ version: '10' }).setToken(this.token);
-            try {
-                const commands = [];
-                for (let i = 0; i < this.classes.length; i++) {
-                    const classe = this.classes[i];
-                    const slashCommands = yield classe.DefineSlashCommands();
-                    commands.push(...slashCommands);
-                }
-                yield rest.put(v10_1.Routes.applicationCommands(this.client.user.id), { body: commands });
-                console.log('SlashCommands saved');
-            }
-            catch (error) {
-                console.error(error);
-            }
-        });
+            await rest.put(Routes.applicationCommands(this.client.user.id), { body: commands });
+            console.log('SlashCommands saved');
+        }
+        catch (error) {
+            console.error(error);
+        }
     }
     ;
 }
-exports.default = OxyBot;
+export { OxyBotFeature };
+export default OxyBot;
--- dist/src/feature.js
+++ dist/src/feature.js
@@ -1,24 +1,11 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 class OxyBotFeature {
-    constructor() {
-        this.bot = null;
-        /** If it has at least one id, only messages from these channels will be sent */
-        this.channelWhitelist = [];
-        /** Messages from these channels will be ignored (priority) */
-        this.channelBlacklist = [];
-        /** Elements for transferring events to child classes */
-        this.childs = [];
-    }
+    bot = null;
+    /** If it has at least one id, only messages from these channels will be sent */
+    channelWhitelist = [];
+    /** Messages from these channels will be ignored (priority) */
+    channelBlacklist = [];
+    /** Elements for transferring events to child classes */
+    childs = [];
     /**
      * @description Called when bot is started
      */
@@ -26,36 +13,28 @@ class OxyBotFeature {
     /**
      * @description Called before bot closing
      */
-    onClose() {
-        return __awaiter(this, void 0, void 0, function* () { });
-    }
+    async onClose() { }
     /**
      * @description Return slash command to add to bot
      */
-    DefineSlashCommands() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return [];
-        });
+    async DefineSlashCommands() {
+        return [];
     }
     /**
      * @description Called when a message is sended and pass through filters
      * @returns True if message is interpreted
      */
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    onMessage(message) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return false;
-        });
+    async onMessage(message) {
+        return false;
     }
     /**
      * @description Called when a interaction is sended and pass through filters
      * @returns True if interaction is interpreted
      */
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    onInteraction(interaction) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return false;
-        });
+    async onInteraction(interaction) {
+        return false;
     }
     /*************************************/
     /**        PRIVATE FUNCTIONS        **/
@@ -64,49 +43,41 @@ class OxyBotFeature {
      * Called when a message is sended and call event if message pass through filter functions
      * @returns True if message is interpreted
      */
-    parseMessage(message) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let output = this.filter(message.channelId) && (yield this.onMessage(message));
-            output || (output = yield this.parseChilds(message));
-            return output;
-        });
+    async parseMessage(message) {
+        let output = this.filter(message.channelId) && await this.onMessage(message);
+        output ||= await this.parseChilds(message);
+        return output;
     }
-    parseChilds(message) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let output = false;
-            for (let c = 0; c < this.childs.length; c++) {
-                const child = this.childs[c];
-                const childParse = child.parseMessage.bind(child);
-                if (yield childParse(message)) {
-                    output = true;
-                }
+    async parseChilds(message) {
+        let output = false;
+        for (let c = 0; c < this.childs.length; c++) {
+            const child = this.childs[c];
+            const childParse = child.parseMessage.bind(child);
+            if (await childParse(message)) {
+                output = true;
             }
-            return output;
-        });
+        }
+        return output;
     }
     /**
      * Called when an interaction is sended and call event if interaction pass through filter functions
      * @returns True if interaction is interpreted
      */
-    parseInteraction(interaction) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let output = this.filter(interaction.channelId) && (yield this.onInteraction(interaction));
-            output || (output = yield this.parseInteractionChilds(interaction));
-            return output;
-        });
+    async parseInteraction(interaction) {
+        let output = this.filter(interaction.channelId) && await this.onInteraction(interaction);
+        output ||= await this.parseInteractionChilds(interaction);
+        return output;
     }
-    parseInteractionChilds(interaction) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let output = false;
-            for (let c = 0; c < this.childs.length; c++) {
-                const child = this.childs[c];
-                const parseInteractionChild = child.parseInteraction.bind(child);
-                if (yield parseInteractionChild(interaction)) {
-                    output = true;
-                }
+    async parseInteractionChilds(interaction) {
+        let output = false;
+        for (let c = 0; c < this.childs.length; c++) {
+            const child = this.childs[c];
+            const parseInteractionChild = child.parseInteraction.bind(child);
+            if (await parseInteractionChild(interaction)) {
+                output = true;
             }
-            return output;
-        });
+        }
+        return output;
     }
     /**
      * @returns True if channel isn't in blacklist or it's in whitelist
@@ -119,8 +90,8 @@ class OxyBotFeature {
         if (this.channelWhitelist.length) {
             keep = this.channelWhitelist.includes(channelID);
         }
-        keep && (keep = !this.channelBlacklist.includes(channelID));
+        keep &&= !this.channelBlacklist.includes(channelID);
         return keep;
     }
 }
-exports.default = OxyBotFeature;
+export default OxyBotFeature;
